0af91170644ca499e0163d28626e7e9c1666a91f
amdymstfa <amdymoustapha011@gmail.com>
2025-04-11 15:47:18 +0100

    Feat(migrations): implements tables of migration

---
+++ backend/database/migrations/2025_04_11_142142_create_subscribes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('subscribers', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique();
+            $table->timestamps();
+        });
+
+        Schema::create('newsletter_subscriber', function (Blueprint $table) {
+            $table->foreignId('newsletter_id')->constrained();
+            $table->foreignId('subscriber_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscribes');
+    }
+};
