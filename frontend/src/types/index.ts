a63b84ee46f4fc4af61f67d961f04086db6ec19d
amdymstfa <amdymoustapha011@gmail.com>
2025-04-13 15:45:03 +0100

    implements interface of new letter and scribers

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,14 @@
+export interface Newsletter {
+    id: number;
+    title: string;
+    content: string;
+    created_at: string;
+    updated_at: string;
+  }
+  
+  export interface Subscriber {
+    id: number;
+    email: string;
+    created_at: string;
+  }
+  
